b114f336954ab61f39a6391db9ac360d9eaeecb4
alonsopinoo <63528723+alonsopinoo@users.noreply.github.com>
2020-07-24 03:43:38 -0400

    lector txt

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', inicio, false);
+
+function inicio() {
+    document.getElementById('archivo').addEventListener('change', cargar, false);               
+}
+
+function cargar(ev) {
+    var arch=new FileReader();
+    arch.addEventListener('load',leer,false);
+    arch.readAsText(ev.target.files[0]);
+}
+
+function leer(ev) {
+    document.getElementById('editor').value=ev.target.result;
+}
