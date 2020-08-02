439c5e6ec38e26ed96cc8046daac95131175f156
Leonardo Peña <40183878+LeoQuark@users.noreply.github.com>
2020-08-02 03:09:48 -0400

    arreglitos

--- js/prueba.js
+++ js/prueba.js
@@ -1,30 +1,40 @@
 const addTexto = document.querySelector("#enviar");
 addTexto.addEventListener("click", () => {
   const texto = document.querySelector("#editor").value;
-  const separar = (str) => {
-    let aux = str.split(" ");
-    let aux1 = aux[0];
-    let aux2 = aux1.replace(/\n|\r/g, "");
-    let total = aux2.split(";");
-    return total;
-  };
-  // textoPlano es un array con cada valor del txt
-  var textoPlano = separar(texto);
-  // console.log("textoPano: ",textoPlano);
-  var listaValores = [], cont = 0, aux = [];
-  for (let i = 0; i < textoPlano.length; i++) {
-    let coor = [];
-    if (cont == 2) {
-      coor.push(textoPlano[i].split(","));
-      aux.push(textoPlano[i].split(","));
-      listaValores.push(aux);
-      cont = 0;
-      aux = [];
-    } else {
-      aux.push(textoPlano[i]);
-      cont++;
+  if (texto === "" || texto === null || texto === undefined) {
+    console.log("uwu");
+  } else {
+    const separar = (str) => {
+      let aux = str.split(" ");
+      let aux1 = aux[0];
+      let aux2 = aux1.replace(/\n|\r/g, "");
+      let total = aux2.split(";");
+      return total;
+    };
+    // textoPlano es un array con cada valor del txt
+    var textoPlano = separar(texto);
+    // console.log("textoPano: ",textoPlano);
+    var listaValores = [], cont = 0, aux = [];
+    for (let i = 0; i < textoPlano.length; i++) {
+      let coor = [];
+      if (cont == 2) {
+        coor.push(textoPlano[i].split(","));
+        aux.push(textoPlano[i].split(","));
+        listaValores.push(aux);
+        cont = 0;
+        aux = [];
+      } else {
+        aux.push(textoPlano[i]);
+        cont++;
+      }
     }
+    console.log("listaValores", listaValores);
+    const showCoordenadas = document.querySelector('.showCoordenadas'),
+      showHojaDeRuta = document.querySelector('.showHojaDeRuta');
+
+    showCoordenadas.style.display = "none";
+    showHojaDeRuta.style.display = "block";
+    addTexto.disbled = true;
   }
-  console.log("listaValores", listaValores);
 });
-console.log("listaValores", listaValores);
+
