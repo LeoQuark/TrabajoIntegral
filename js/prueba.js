5a943d316f3ffe75fd598b5ed0bc591bfed9638a
Valentina Alarcón <63269085+valentinalarcon@users.noreply.github.com>
2020-08-02 21:05:14 -0400

    ruta

--- js/prueba.js
+++ js/prueba.js
@@ -37,4 +37,68 @@ addTexto.addEventListener("click", () => {
     addTexto.disbled = true;
   }
 });
+//DECLARACION DEL OBJETO 
+function Punto(nombre,coordenada){
+  this.nombre = nombre,	
+  this.coordenada = coordenada
+}
+//EJEMPLOS
+var Ventas = []
+ Ventas[0]= new Punto(["870"],["24","47"])	
+ Ventas[1]= new Punto(["420"],["29","9"])	
+ Ventas[2]= new Punto(["345"],["30","52"])	
 
+
+var Centros= new Punto(["300"],["12","58"])	
+
+//console.log(Ventas)
+//console.log(Centros)
+
+
+//var Camiones = [], PtosVenta = [], Centros = []
+
+
+function Hipotenusas(CordenadaInicial, Ventas){
+  var hip = [], x ,y, suma, h
+  //console.log(Ventas[0].coordenada)
+  for(let i=0;i<Ventas.length;i++){
+    x = CordenadaInicial[0] - Ventas[i].coordenada[0]
+    y = CordenadaInicial[1] - Ventas[i].coordenada[1]
+    x = Math.abs(x), y =  Math.abs(y)
+    suma = Math.pow(x,2) + Math.pow(y,2)
+    h = Math.sqrt(suma)
+    h= h.toFixed(5)
+    hip.push(h)
+  }
+  return hip;
+}
+function Menor(hipotenusas){
+  var indice = 0, aux = hipotenusas[0]
+  for(let i=0;i<hipotenusas.length;i++){
+    var n = parseInt(aux, 10), m = parseInt(hipotenusas[i], 10);
+    if(n >= m){
+      aux = hipotenusas[i]
+      indice = i
+    }
+  }
+  //console.log(aux,indice)
+  var menor_hip = new Object()
+  menor_hip.indice = indice
+  menor_hip.hip = aux
+  return menor_hip;
+}
+
+function Ruta(Ventas, Centros){
+  var ruta = []
+  var ventas_aux = Centros.coordenada
+  var lista_ventas = JSON.parse(JSON.stringify(Ventas))
+  for(let i=0;i < Ventas.length;i++){
+    var hip = Hipotenusas(ventas_aux,lista_ventas)
+    var menor = Menor(hip)
+    ruta.push(lista_ventas[menor.indice])
+    ventas_aux = lista_ventas[menor.indice].coordenada
+    lista_ventas.splice(menor.indice,1)
+  }
+  console.log(ruta)
+}
+Ruta(Ventas,Centros)
